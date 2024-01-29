017fd711990fc73781b08c726ce3c02703770788
rickmli <rmli@usc.edu>
2024-01-29 13:40:14 -0800

    create signin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -26,7 +27,35 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.json({ message: "Signup successfull" });
+    res.json({ message: "Signup successful" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password || username === "" || password === "")
+    next(errorHandler(400, "All fields are required."));
+
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) next(errorHandler(404, "User not found."));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) next(errorHandler(400, "Invalid password."));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRETE, {
+      expiresIn: "15m",
+    });
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
   } catch (err) {
     next(err);
   }
