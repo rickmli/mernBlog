7bce833ec70b1e78a3b576a5528ef6d419d3649e
rickmli <rmli@usc.edu>
2024-01-28 19:43:15 -0800

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.MONGODB);
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log("Mongodb is connected."))
+  .catch((err) => console.log(err));
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000.");
 });
