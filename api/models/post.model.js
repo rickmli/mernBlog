d30376128a79526a0b83127852ffc75e2b67be38
rickmli <rmli@usc.edu>
2024-02-04 18:16:55 -0800

    add create post api route

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://cdn.shopify.com/s/files/1/0428/9455/7352/files/Pix_4_A01_render-min.jpg?v=1656512461",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
