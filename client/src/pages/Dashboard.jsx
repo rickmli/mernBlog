b4a8052c2aaff917390357ecebf123e119930060
rickmli <rmli@usc.edu>
2024-02-06 16:16:28 -0800

    add dashboard overview to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
 import DashComments from "../components/DashComments";
+import DashChart from "../components/DashChart";
 
 function Dashboard() {
   const [tab, setTab] = useState("");
@@ -24,10 +25,15 @@ function Dashboard() {
         {/* Sidebar */}
         <DashSidebar />
       </div>
+      {/* Dashboard */}
+      {tab === "dash" && <DashChart />}
       {/* Profile */}
       {tab === "profile" && <DashProfile />}
+      {/* Posts */}
       {tab === "posts" && <DashPosts />}
+      {/* Users */}
       {tab === "users" && <DashUsers />}
+      {/* Comments */}
       {tab === "comments" && <DashComments />}
     </div>
   );
